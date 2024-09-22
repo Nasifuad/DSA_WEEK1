cbd8877a9039a0c29b4769851c62edb967e75d52
Nasif Fuad <nasifuad007@gmail.com>
2024-09-22 07:52:36 +0600

    lowest

--- array.js
+++ array.js
@@ -4,7 +4,7 @@ let prev = 0;
 
 console.log(cur, prev);
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 1; i++) {
   let next = cur + prev;
   console.log(next);
   prev = cur;
@@ -26,4 +26,21 @@ const Fibonacci = (n) => {
     Fibonacci(n);
   }
 };
-Fibonacci(20);
+Fibonacci(1);
+
+//Lowest Value from the array
+let a = Math.floor(Math.random() * 100);
+let b = Math.floor(Math.random() * 100);
+let c = Math.floor(Math.random() * 100);
+let d = Math.floor(Math.random() * 100);
+let e = Math.floor(Math.random() * 100);
+let f = Math.floor(Math.random() * 100);
+let g = Math.floor(Math.random() * 100);
+let arr = [a, b, c, d, e, f, g];
+let min = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < min) {
+    min = arr[i];
+  }
+}
+console.log(`The lowers in the ${arr} is ${min}`);
