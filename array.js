183abe72ac9e9b547f5b60867438482e1a010ae6
Nasif Fuad <nasifuad007@gmail.com>
2024-09-22 07:41:59 +0600

    recursion

--- array.js
+++ array.js
@@ -10,3 +10,20 @@ for (let i = 0; i < 20; i++) {
   prev = cur;
   cur = next;
 }
+// Using Recursion
+
+console.log(0, 1);
+count = 2;
+let cur1 = 1;
+let prev1 = 0;
+const Fibonacci = (n) => {
+  while (count < n) {
+    let next = cur1 + prev1;
+    console.log(next);
+    prev1 = cur1;
+    cur1 = next;
+    count++;
+    Fibonacci(n);
+  }
+};
+Fibonacci(20);
